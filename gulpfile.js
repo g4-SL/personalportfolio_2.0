f1eafa6383ca4e9f8734b02ed476045a14cc20a7
g4-SL <slouise@live.com.my>
2016-02-05 16:37:16 +0800

    Changed gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync');
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
+var del = require('del');
+var runSequence = require('run-sequence');
 var neat = require('node-neat').includePaths;
 
 // Development Tasks 
@@ -15,8 +17,7 @@ gulp.task('browserSync', function() {
   browserSync({
     server: {
       baseDir: 'app'
-    },
-    proxy: 'localhost:8888'
+    }
   });
 });
 
@@ -55,4 +56,32 @@ gulp.task('useref', function() {
 gulp.task('fonts', function() {
   return gulp.src('app/fonts/**/*')
     .pipe(gulp.dest('dist/fonts'));
+});
+
+// Cleaning 
+gulp.task('clean', function() {
+  return del.sync('dist').then(function(cb) {
+    return cache.clearAll(cb);
+  });
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+});
+
+// Build Sequences
+// ---------------
+
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'browserSync', 'watch'],
+    callback
+  );
+});
+
+gulp.task('build', function(callback) {
+  runSequence(
+    'clean:dist',
+    ['sass', 'useref', 'fonts'],
+    callback
+  );
 });
