5a2c9c6f5a695b82c832f83d5b62c8a8d331dbeb
g4-SL <slouise@live.com.my>
2016-02-05 10:53:22 +0800

    Added bourbon and neat dependency

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence');
+var neat = require('node-neat').includePaths;
 
 // Development Tasks 
 // -----------------
@@ -27,7 +28,9 @@ gulp.task('browserSync', function() {
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass()) // Passes it through a gulp-sass
+    .pipe(sass({
+      includePaths: ['sass'].concat(neat)
+    })) // Passes it through a gulp-sass
     .pipe(gulp.dest('app/css')) // Outputs it in the css folder
     .pipe(browserSync.reload({ // Reloading with Browser Sync
       stream: true
