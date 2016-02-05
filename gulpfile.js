cb0329afaa38d591b24a9546978a7449dd53bc28
g4-SL <slouise@live.com.my>
2016-02-05 15:03:41 +0800

    Reduce gulpfile and added some backbone code html

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
-var imagemin = require('gulp-imagemin');
-var cache = require('gulp-cache');
-var del = require('del');
-var runSequence = require('run-sequence');
 var neat = require('node-neat').includePaths;
 
 // Development Tasks 
@@ -57,46 +51,8 @@ gulp.task('useref', function() {
     .pipe(gulp.dest('dist'));
 });
 
-// Optimizing Images 
-gulp.task('images', function() {
-  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-    // Caching images that ran through imagemin
-    .pipe(cache(imagemin({
-      interlaced: true,
-    })))
-    .pipe(gulp.dest('dist/images'));
-});
-
 // Copying fonts 
 gulp.task('fonts', function() {
   return gulp.src('app/fonts/**/*')
     .pipe(gulp.dest('dist/fonts'));
-});
-
-// Cleaning 
-gulp.task('clean', function() {
-  return del.sync('dist').then(function(cb) {
-    return cache.clearAll(cb);
-  });
-});
-
-gulp.task('clean:dist', function() {
-  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
-});
-
-// Build Sequences
-// ---------------
-
-gulp.task('default', function(callback) {
-  runSequence(['sass', 'browserSync', 'watch'],
-    callback
-  );
-});
-
-gulp.task('build', function(callback) {
-  runSequence(
-    'clean:dist',
-    ['sass', 'useref', 'images', 'fonts'],
-    callback
-  );
 });
