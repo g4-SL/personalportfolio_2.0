4174596713048c8dd69a3907637a7cb1399fed55
g4-SL <slouise@live.com.my>
2016-02-08 15:45:46 +0800

    Added form template and changed header and added a default work page

---
+++ app/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Sarah Louise</title>
+	<meta name="description" content="Personal Portfolio">
+	<meta name="author" content="Sarah Louise">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!--build:css css/styles.min.css--> 
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!--endbuild-->
+</head>
+<body>
