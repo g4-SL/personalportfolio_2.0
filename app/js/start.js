843eed71b6cbb73c44ce651436c2c7f4e20d69db
g4-SL <slouise@live.com.my>
2016-03-11 16:55:11 +0800

    removed sticky header and implemented modal dialog for projects

--- app/js/start.js
+++ app/js/start.js
@@ -1,33 +1,10 @@
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top - 52
-        }, 500);
-        return false;
-      }
-    }
-  });
+$('.view-project').on('click', function(){
+  modal = $(this).attr('data-modal-target');
+  $('#' + modal).addClass('show');
+  $('body').addClass('no-scroll');
 });
 
-$(document).ready(function(){
-  var bottom = $('header nav').offset().top + $('header nav').outerHeight();
-  $('header nav ul li a').on('click', function(e) {
-    e.preventDefault();
-  });
-  
-  $(window).on('scroll',function() {
-    var scrolltop = $(this).scrollTop();
- 
-    if(scrolltop >= bottom) {
-      $('header .container').addClass('sticky');
-    }
-    
-    else if(scrolltop <= bottom) {
-      $('header .container').removeClass('sticky');
-    }
-  });
+$('.modal-box .close').on('click', function(){
+  $(this).closest('.modal-box').removeClass('show');
+  $('body').removeClass('no-scroll');
 });
