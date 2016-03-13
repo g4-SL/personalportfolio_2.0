115fc09577799db36b22a1986808ef4fc0674f82
g4-SL <slouise@live.com.my>
2016-03-14 00:34:40 +0800

    Changed layout design, sticky header with hamburger icon, php for mail form, updated content, changed modal styling for project item

--- app/js/start.js
+++ app/js/start.js
@@ -1,10 +1,51 @@
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if($('nav').hasClass('open')){
+      $('nav').removeClass('open');
+    }
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 90
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
+
 $('.view-project').on('click', function(){
   modal = $(this).attr('data-modal-target');
   $('#' + modal).addClass('show');
   $('body').addClass('no-scroll');
 });
 
-$('.modal-box .close').on('click', function(){
+$('.modal-box .modal-close').on('click', function(){
   $(this).closest('.modal-box').removeClass('show');
   $('body').removeClass('no-scroll');
+});
+
+$('.nav-link').on('click', function(){
+  $(this).parent().siblings('nav').toggleClass('open');
+});
+
+$(document).ready(function(){
+  var bottom = $('header').offset().top + $('header').outerHeight();
+  $('header nav ul li a').on('click', function(e) {
+    e.preventDefault();
+  });
+  
+  $(window).on('scroll',function() {
+    var scrolltop = $(this).scrollTop();
+ 
+    if(scrolltop >= bottom) {
+      $('header').addClass('sticky');
+    }
+    
+    else if(scrolltop <= bottom) {
+      $('header').removeClass('sticky');
+    }
+  });
 });
